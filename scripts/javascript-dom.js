e49dbff92e360a4bd8437c2bb6cbab77b2bacaf3
Andrew Molten <andrewmolten23@gmail.com>
2024-04-05 14:22:40 +1300

    edit javascripts relationship to HTML & CSS paragraph

--- scripts/javascript-dom.js
+++ scripts/javascript-dom.js
@@ -1,12 +1,40 @@
 //
 //
 
-function openUp() {
-  turnOnLights()
-  putMusicOn()
-  setUpCashRegister()
-  flipSignToOpen()
-  unlockFrontDoor()
+// function openUp() {
+//   turnOnLights()
+//   putMusicOn()
+//   setUpCashRegister()
+//   flipSignToOpen()
+//   unlockFrontDoor()
+// }
+
+// openUp()
+
+let lawn = [5, 5, 6, 5, 'lawn chair', 5, 5, 7, 7, 5, 'clothes peg']
+
+const lawnMower = {
+  catcher: 0,
+  petrolTank: 2,
 }
 
-openUp()
+function mowLawns() {
+  checkPetrol(lawnMower)
+  getLawnmowerOut()
+  startLawnmower()
+  pushLawnMower(lawn)
+}
+
+function checkPetrol(lawnMower) {
+  lawnMower.petrolTank < 3 ? fillLawnMower() : ''
+}
+
+function fillLawnMower(lawnMower) {
+  getPetrolCan()
+  openLawnmowerCap()
+  // Pour petrol in
+  for (petrolPoured = 0; lawnMower.petrolTank < 10; petrolPoured++) {
+    lawnMower.petrolTank++
+  }
+  replaceCap()
+}
