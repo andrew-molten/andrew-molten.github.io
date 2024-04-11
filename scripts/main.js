02d2013041fcd769ced12c2e3fb2efdd4c8e8c11
Andrew Molten <andrewmolten23@gmail.com>
2024-04-11 16:37:49 +1200

    create function to show blog-link images

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,18 @@
 const coreBlogPosts = document.getElementById('core-blog-posts').children
+const techBlogPosts = document.getElementById('tech-blog-posts').children
 
-function hideBlogPostImages() {
-  for (post of coreBlogPosts) {
-    post.firstElementChild.children[0].classList += ' hide-image'
+function hideBlogPostImages(posts) {
+  for (Element of posts) {
+    Element.firstElementChild.children[0].classList += ' hide-image'
   }
 }
 
-// hideBlogPostImages()
+function showBlogPostImages(posts) {
+  for (Element of posts) {
+    Element.firstElementChild.children[0].classList -= ' hide-image'
+  }
+}
+
+// hideBlogPostImages(coreBlogPosts)
+// hideBlogPostImages(techBlogPosts)
+// showBlogPostImages(techBlogPosts)
