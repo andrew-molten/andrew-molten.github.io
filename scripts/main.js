2bda050bad608b03e51836737da80aaeb52bdf7d
Andrew Molten <andrewmolten23@gmail.com>
2024-04-11 16:57:17 +1200

    add buttons to hide or show blog-link images

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,23 @@
+//////////////////////////////////////////////////////////////////////////////
+//////////////////// HIDE OR SHOW BLOG POST IMAGES ///////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
 const coreBlogPosts = document.getElementById('core-blog-posts').children
 const techBlogPosts = document.getElementById('tech-blog-posts').children
+const hideTechBtn = document.getElementById('hide-tech-images')
+const hideCoreBtn = document.getElementById('hide-core-images')
+
+hideTechBtn.addEventListener('click', hideCoreOrTechImages)
+hideCoreBtn.addEventListener('click', hideCoreOrTechImages)
+
+function hideCoreOrTechImages(e) {
+  e.target.id.slice(5, 9) == 'core'
+    ? hideBlogPostImages(coreBlogPosts)
+    : hideBlogPostImages(techBlogPosts)
+}
 
 function hideBlogPostImages(posts) {
   for (Element of posts) {
-    Element.firstElementChild.children[0].classList += ' hide-image'
+    Element.firstElementChild.children[0].classList.toggle('hide-image')
   }
 }
-
-function showBlogPostImages(posts) {
-  for (Element of posts) {
-    Element.firstElementChild.children[0].classList -= ' hide-image'
-  }
-}
-
-// hideBlogPostImages(coreBlogPosts)
-// hideBlogPostImages(techBlogPosts)
-// showBlogPostImages(techBlogPosts)
